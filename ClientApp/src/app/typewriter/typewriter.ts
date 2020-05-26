0f1964bf7110704ad324b6678243df414cf9b0d1
Anton Zhluktarov <anton.zhluktarov@gmail.com>
2020-05-26 06:07:38 +0300

    Concurrency in JS PogU

--- ClientApp/src/app/typewriter/typewriter.ts
+++ ClientApp/src/app/typewriter/typewriter.ts
@@ -5,8 +5,11 @@ export default class Typewriter {
   private period: number;
 
   private isDeleting = false;
+  private changingStarted = false;
+  private shallQuit = false;
 
   private timeoutSubscriptions: number[] = [];
+  private lampartCounter = 0;
 
   constructor(
     typewriterOptions: {
@@ -38,13 +41,51 @@ export default class Typewriter {
     }
   }
 
-  public changeText(newText: string, delay: number = 0) {
-    // Prevent multiple changeTexts running on a single element
+  private queue = [];
+
+  public async changeText(newText: string, delay: number = 0) {
+    if (this.target.innerText === newText) {
+      return;
+    }
+
+
+    // Concurrency check
+    // Honestly, this is a mess
+    const currentLamportCounter = this.lampartCounter++;
+    this.queue.push(currentLamportCounter);
+
+    this.changingStarted = true;
+
+    if (this.queue.length !== 1) {
+      this.shallQuit = true;
+
+      const indexInQueue = this.queue.findIndex(iteratedLamportCounter => iteratedLamportCounter === currentLamportCounter);
+      // If new one has started between start function and this
+      if (indexInQueue !== null) {
+        let lamportQuit = false;
+        while (!lamportQuit && (this.shallQuit || !this.changingStarted)) {
+          // Pause in js
+          await new Promise(r => setTimeout(r, 100));
+
+          // Lamport quit (if queue has no current counter from the closure, it means that new invocation has been)
+          if (this.queue.length > 0 && this.queue[0] > currentLamportCounter) {
+            lamportQuit = true;
+          }
+        }
+
+        if (lamportQuit) {
+          return;
+        }
+      }
+    }
+    this.shallQuit = false;
+
     this.clearSubscriptions();
+    // Prevent multiple changeTexts running on a single element
     this.isDeleting = !newText.startsWith(this.currentText);
 
     const delayTimeoutSubscription = setTimeout(() => {
-      this.innerChangeText(newText);
+      this.innerChangeText(newText, currentLamportCounter);
 
       this.timeoutSubscriptions = this.timeoutSubscriptions.splice(
         this.timeoutSubscriptions.findIndex(timeoutSubscription => timeoutSubscription === delayTimeoutSubscription),
@@ -55,13 +96,21 @@ export default class Typewriter {
     this.timeoutSubscriptions.push(delayTimeoutSubscription);
   }
 
-  private innerChangeText(desiredText: string): void {
+  private innerChangeText(desiredText: string, lamportCounter: number): void {
+    // Concurrency quit
+    if (this.shallQuit) {
+      this.shallQuit = false;
+
+      this.queue = this.queue.filter(lamport => lamport !== lamportCounter);
+
+      return;
+    }
+
     if (this.isDeleting) {
       this.currentText = this.currentText.substr(0, this.currentText.length - 1);
     } else {
       this.currentText = desiredText.substring(0, this.currentText.length + 1);
     }
-
     this.target.innerText = this.constructInnerText();
 
     let delta = 300 - Math.random() * 100;
@@ -77,7 +126,7 @@ export default class Typewriter {
 
     if (this.currentText !== desiredText) {
       const permutateTextSubscription = setTimeout(() => {
-        this.innerChangeText(desiredText);
+        this.innerChangeText(desiredText, lamportCounter);
 
         this.timeoutSubscriptions = this.timeoutSubscriptions.splice(
           this.timeoutSubscriptions.findIndex(timeoutSubscription => timeoutSubscription === permutateTextSubscription),
@@ -85,6 +134,14 @@ export default class Typewriter {
         );
       }, delta);
       this.timeoutSubscriptions.push(permutateTextSubscription);
+    } else {
+      // Cleanup
+
+      this.changingStarted = false;
+      this.shallQuit = false;
+
+      this.queue = this.queue.filter(lamport => lamport !== lamportCounter);
+      console.log(lamportCounter, this);
     }
   }
 
