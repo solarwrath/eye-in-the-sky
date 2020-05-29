e4f66165daf95c58b90f647368dd2a21e5b7cc0a
Anton Zhluktarov <anton.zhluktarov@gmail.com>
2020-05-29 07:54:09 +0300

    Auth status enum now in string form, can actually read in ngrx store devtools Pog

--- ClientApp/src/app/models/auth-status.enum.ts
+++ ClientApp/src/app/models/auth-status.enum.ts
@@ -1,7 +1,7 @@
 const enum AuthStatus {
-  NOT_LOGGED_IN,
-  PENDING,
-  LOGGED_IN
+  NOT_LOGGED_IN = 'NOT_LOGGED_IN',
+  PENDING = 'PENDING',
+  LOGGED_IN = 'LOGGED_IN',
 }
 
 export default AuthStatus;
