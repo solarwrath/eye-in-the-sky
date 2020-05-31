1dffe4da42b93c2bc053364ee8ecac8fba54b22a
Anton Zhluktarov <anton.zhluktarov@gmail.com>
2020-05-31 08:17:17 +0300

    Loader prettier

--- ClientApp/src/app/components/auth/auth.component.ts
+++ ClientApp/src/app/components/auth/auth.component.ts
@@ -46,17 +46,14 @@ import SignUpStatus from '../../core/models/sign-up-status.enum';
         transition(':enter', [
           style({
             opacity: 0,
-            transform: 'scale(0)'
           }),
           animate('500ms', style({
             opacity: 1,
-            transform: 'scale(1)'
           })),
         ]),
         transition(':leave', [
           animate('500ms', style({
             opacity: 0,
-            transform: 'scale(0)'
           }))
         ])
       ])
